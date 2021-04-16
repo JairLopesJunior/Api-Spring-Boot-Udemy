c4eaad9d96f295129cb3913052800df6140d5201
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-16 12:56:40 -0300

    Trabalhando com multiplos ambientes e profiles

--- api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/MinhaConfiguration.java
+++ api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/MinhaConfiguration.java
@@ -1,13 +1,18 @@
 package api.spring.boot.udemy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Vendas";
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
     }
 }
