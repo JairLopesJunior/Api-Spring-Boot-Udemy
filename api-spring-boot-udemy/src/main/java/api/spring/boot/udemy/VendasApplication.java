ca100190e6fad6971321889fa1538f6a2d01da97
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-18 18:06:52 -0300

    Refatorando as outras operações para o  JPA

--- api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/VendasApplication.java
+++ api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/VendasApplication.java
@@ -32,25 +32,25 @@ public class VendasApplication {
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
 
-//            todosClientes.forEach(c -> {
-//                c.setNome(c.getNome() + " atualizado");
-//                clientes.atualizar(c);
-//            });
-//
-//            System.out.println("Buscando por nome:");
-//            clientes.buscarPorNome("oa").forEach(System.out::println);
-//
-//            System.out.println("Deletando:");
-//            clientes.obterTodos().forEach(c -> {
-//                clientes.delete(c);
-//            });
-//
-//            todosClientes = clientes.obterTodos();
-//            if(todosClientes.isEmpty()){
-//                System.out.println("Nenhum cliente encontrado");
-//            }else{
-//                todosClientes.forEach(System.out::println);
-//            }
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado");
+                clientes.atualizar(c);
+            });
+
+            System.out.println("Buscando por nome:");
+            clientes.buscarPorNome("oa").forEach(System.out::println);
+
+            System.out.println("Deletando:");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado");
+            }else{
+                todosClientes.forEach(System.out::println);
+            }
         };
     }
 
