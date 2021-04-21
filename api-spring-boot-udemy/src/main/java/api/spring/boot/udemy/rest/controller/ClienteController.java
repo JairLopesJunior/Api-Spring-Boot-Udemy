46241c50ccb3b04a9249e4634205f9778f87b7c1
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-21 15:00:12 -0300

    Mapeando as Requisições com Request Mapping

--- api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/rest/controller/ClienteController.java
+++ api-spring-boot-udemy/src/main/java/api/spring/boot/udemy/rest/controller/ClienteController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello/{nome}",
+                    method = RequestMethod.GET,
+                    consumes = {"application/json","application/xml"},
+                    produces = {"application/json","application/xml"})
     @ResponseBody
     public String hello(@PathVariable("nome") String nomeCliente){
         return String.format("Hello %s", nomeCliente);
