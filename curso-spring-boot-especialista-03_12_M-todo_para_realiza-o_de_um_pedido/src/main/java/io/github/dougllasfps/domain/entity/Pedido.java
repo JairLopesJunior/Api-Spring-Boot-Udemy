ea8d1e519786fef201aff4e89471f539011bd421
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-26 21:20:43 -0300

    Criando o status do Pedido

--- curso-spring-boot-especialista-03_12_M-todo_para_realiza-o_de_um_pedido/src/main/java/io/github/dougllasfps/domain/entity/Pedido.java
+++ curso-spring-boot-especialista-03_12_M-todo_para_realiza-o_de_um_pedido/src/main/java/io/github/dougllasfps/domain/entity/Pedido.java
@@ -1,5 +1,6 @@
 package io.github.dougllasfps.domain.entity;
 
+import io.github.dougllasfps.domain.enums.StatusPedido;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -31,6 +31,10 @@ public class Pedido {
 
     @Column(name = "total", precision = 20, scale = 2)
     private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusPedido status;
 
     @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itens;
--- curso-spring-boot-especialista-03_12_M-todo_para_realiza-o_de_um_pedido/src/main/java/io/github/dougllasfps/service/impl/PedidoServiceImpl.java
+++ curso-spring-boot-especialista-03_12_M-todo_para_realiza-o_de_um_pedido/src/main/java/io/github/dougllasfps/service/impl/PedidoServiceImpl.java
@@ -4,6 +4,7 @@ import io.github.dougllasfps.domain.entity.Cliente;
 import io.github.dougllasfps.domain.entity.ItemPedido;
 import io.github.dougllasfps.domain.entity.Pedido;
 import io.github.dougllasfps.domain.entity.Produto;
+import io.github.dougllasfps.domain.enums.StatusPedido;
 import io.github.dougllasfps.domain.repository.Clientes;
 import io.github.dougllasfps.domain.repository.ItemsPedido;
 import io.github.dougllasfps.domain.repository.Pedidos;
@@ -42,6 +43,7 @@ public class PedidoServiceImpl implements PedidoService {
         pedido.setTotal(dto.getTotal());
         pedido.setDataPedido(LocalDate.now());
         pedido.setCliente(cliente);
+        pedido.setStatus(StatusPedido.REALIZADO);
 
         List<ItemPedido> itemsPedido = converterItems(pedido, dto.getItems());
         repository.save(pedido);
