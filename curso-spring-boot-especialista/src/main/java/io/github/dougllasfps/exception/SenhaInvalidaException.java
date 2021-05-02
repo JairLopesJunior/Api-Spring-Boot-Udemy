7357f4eba4c751ef7645251879a791cb90703853
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-02 16:59:20 -0300

    Fazendo a  migração para o Banco de Dados MySQL

--- curso-spring-boot-especialista/src/main/java/io/github/dougllasfps/exception/SenhaInvalidaException.java
+++ curso-spring-boot-especialista/src/main/java/io/github/dougllasfps/exception/SenhaInvalidaException.java
@@ -3,6 +3,6 @@ package io.github.dougllasfps.exception;
 public class SenhaInvalidaException extends RuntimeException{
 
     public SenhaInvalidaException(){
-        super();
+        super("Senha inválida");
     }
 }
