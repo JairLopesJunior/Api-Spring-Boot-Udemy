3674639076f171a39fe3dbb02e4148eeb11a0f57
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-02 14:22:32 -0300

    Implementando o  método  de autenticação de usuários

---
+++ curso-spring-boot-especialista/src/main/java/io/github/dougllasfps/rest/dto/CredentiaisDTO.java
@@ -0,0 +1,12 @@
+package io.github.dougllasfps.rest.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredentiaisDTO {
+
+    private String login;
+    private String senha;
+}
